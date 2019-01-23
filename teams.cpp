c5faee63db0ab6a428898bda98f1d4a37e998d5d
Cameron Baab <baab@usc.edu>
2019-01-22 23:29:50 -0800

    Commit

---
+++ teams.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+// *You* are not allowed to use global variables
+//  but for just the output portion *we* will. ;>
+int combo = 1;
+
+// @brief Prints a single combination of teams
+//
+// @param[in] team1 Array containing the names of team 1
+// @param[in] team2 Array containing the names of team 2
+// @param[in] len Size of each array
+void printSolution(const string* team1, 
+		   const string* team2,
+		   int len)
+{
+  cout << "\nCombination " << combo++ << endl;
+  cout << "T1: ";
+  for(int i=0; i < len; i++){
+    cout << team1[i] << " ";
+  }
+  cout << endl;
+  cout << "T2: ";
+  for(int i=0; i < len; i++){
+    cout << team2[i] << " ";
+  }
+  cout << endl;
+}
+
+string getTeam2(string data, string team1, int team_size){
+  string * team1_d = (string*) calloc (team_size, sizeof(string)) ;
+  string* copy = team1_d ;
+  istringstream iss (team1) ;
+  for (int i = 0 ; i < team_size; i++){
+    string c ;
+    iss >> c;
+    *copy = c ;
+    copy++ ;
+  }
+
+  string team2 ;
+  istringstream all (data) ;
+  string curr1 ;
+  string curr2 ;
+  bool both = false;
+  while(1){
+    all >> curr1 ;
+    if(all.fail()) break ;
+    for (int i = 0; i < team_size; i++){
+      curr2 = team1_d[i];
+      if(curr1 == curr2){
+        both = true ;
+      }
+    }
+    if(!both){
+      team2 = team2 + " " + curr1 ;
+    }
+    both = false ;
+  }
+
+  free(team1_d) ;
+  return team2 ;
+}
+// You may add additional functions here
+int generateComb(string* data, string team1, int curr_size, int depth, int team_size, string all){
+  string element ;
+  if (depth != team_size*2){
+    element = *data ;
+    data++ ;
+    depth++ ;
+  }
+
+  //first element in team 1:
+  if (curr_size == team_size){
+    string temp =  getTeam2(all,team1, team_size) ;
+    string* _team1 = (string*) calloc (team_size, sizeof(string)) ;
+    string* _team2 = (string*) calloc (team_size, sizeof(string)) ;
+    istringstream one (team1) ;
+    istringstream two (temp) ;
+    string c ;
+    for (int i = 0; i < team_size; i++){
+      one >> c ;
+      *_team1 = c ;
+      _team1++ ;
+    }
+    for (int i = 0; i < team_size; i++){
+      two >> c ;
+      *_team2 = c;
+      _team2++ ;
+    }
+    for (int i = 0; i < team_size; i++){
+      _team1-- ;
+      _team2-- ;
+    }
+    const string* _t1 = _team1 ;
+    const string* _t2 = _team2 ;
+    
+    printSolution(_t1,_t2,team_size) ;
+    free(_team1) ;
+    free(_team2) ;
+    one.clear() ;
+    two.clear() ;
+
+  }
+  else{
+    string temp = team1 + " " + element ;
+    //Actually a real element has been added
+    if (element != ""){
+      generateComb(data, temp, curr_size + 1, depth, team_size, all) ;
+    }
+
+    if (depth != team_size*2){
+      generateComb(data, team1, curr_size, depth, team_size, all) ;
+    }
+
+  }
+  return 0;
+
+}
+
+
+int main(int argc, char* argv[])
+{
+  if(argc < 2){
+    cerr << "Please provide a file of names" << endl;
+    return 1;
+  }
+  ifstream ifile(argv[1]);
+  if(ifile.fail()){
+    cerr << "Error" << endl;
+    return 1;
+  }
+  int num ;
+  string name ;
+  ifile >> num ;
+  if(ifile.fail()){
+    cout << "Error" << endl ;
+    return 1 ;
+  } 
+  //Dynamically allocate data
+  string * data ;
+  data = (string*) calloc (num, sizeof(string)) ;
+  string all ;
+  //Read in names and assign them into the data
+  for (int i = 0 ; i < num ; i++){
+    ifile >> name ;
+    //If one of the inputs is not a string, fail
+    if(ifile.fail()){
+      cout << "Error" << endl ;
+      return 1;
+    }
+    all = all + " " + name ;
+    *data = name ;
+    data++;
+  }
+  //reset to the start
+  data = data - num;
+  string team1 = "" ;
+  generateComb(data, team1, 0, 0,  num/2, all) ;
+
+  free(data) ;
+
+
+
+
+
+  return 0;
+}
---
+++ wsearch.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct Location
+{
+  Location()
+  {
+    row = -1;
+    col = -1;
+  }
+  Location(int r, int c)
+  {
+    row = r;
+    col = c;
+  }
+  int row;
+  int col;
+};
+
+// @param[in] instream - Input (file) stream for the input file.  Opened in main()
+// @param[out] grid Blank vector of vectors.  Should be updated with the
+//                  contents of the grid from the input file
+// @return true if successful, false otherwise (see HW writeup for details
+//         about possible error cases to be checked)
+bool readGrid(istream& instream, vector<vector<char> >& grid )
+{
+  vector<string> rows ;
+  string line ;
+  char c ;
+  int stan_rowl ;
+  bool test = true ;
+  while (1){
+    getline(instream,line) ;
+    if(instream.fail()) break ;
+    rows.push_back(line) ;
+  }
+  for(vector<string>::iterator row = rows.begin(); row != rows.end(); ++row) {
+    istringstream iss (row[0]) ;
+    vector<char> line_c ;
+    while (1){
+      iss >> c ;
+      if(iss.fail()) break ;
+      line_c.push_back(c) ;
+    }
+    grid.push_back(line_c) ;
+  }
+  //Check all rows are the same length and above 0
+  stan_rowl = (grid.begin()[0]).size() ;
+  for(vector<vector<char> >::iterator row = grid.begin() ; row != grid.end(); ++row) {
+    if (stan_rowl != (row[0]).size()){
+      test = false ;
+    }
+    if ((row[0]).size() == 0){
+      test = false ;
+    }
+  }
+  return test ;
+
+
+
+}
+
+// prototype - will be implemented below
+bool findWordHelper(
+   const vector<vector<char> >& grid, 
+   Location currLoc,
+   Location delta,
+   string word,
+   unsigned int currWordIndex);
+
+// Do not change
+void printSolution(const string& targetWord, const Location& start, string direction)
+{
+  cout << targetWord << " starts at (row,col) = (" << start.row << "," << start.col
+       << ") and proceeds " << direction << "." << endl;
+}
+
+// Do not change
+void printNoSolution(const string& targetWord)
+{
+  cout << targetWord << " does NOT occur." << endl;
+}
+
+// Complete - you should not need to change this.
+void findWord(const vector<vector<char> >& grid, 
+	      const string& targetWord)
+{
+  bool found = false;
+  if(targetWord.size() < 2){
+    cout << "Need a word of length 2 or more." << endl;
+    return;
+  }
+  for(unsigned int r=0; r < grid.size(); r++){
+    for(unsigned int c=0; c < grid[r].size(); c++){
+      if(grid[r][c] == targetWord[0]){
+	Location loc(r,c);
+	if( findWordHelper(grid, Location(r-1,c), Location(-1, 0), targetWord, 1) ){
+	  printSolution(targetWord, loc, "up");
+	  found = true;
+	}
+	if( findWordHelper(grid, Location(r,c-1), Location(0,-1), targetWord, 1) ){
+	  printSolution(targetWord, loc, "left");
+	  found = true;
+	}
+	if( findWordHelper(grid, Location(r+1,c), Location(1, 0), targetWord, 1) ){
+	  printSolution(targetWord, loc, "down");
+	  found = true;
+	}
+	if( findWordHelper(grid, Location(r,c+1), Location(0,1), targetWord, 1) ){
+	  printSolution(targetWord, loc, "right");
+	  found = true;
+	}
+      }      
+    }
+  }
+  if(!found){
+    printNoSolution(targetWord);
+  }
+}
+
+/**
+ * @param[in] grid The 2D vector containing the entire search contents
+ * @param[in] currLoc Current row and column location to try to match 
+ *                    with the next letter
+ * @param[in] delta   Indicates direction to move for the next search
+ *                    by containing the *change* in row, col values
+ *                    (i.e. 1,0 = down since adding 1 will move down 1 row)
+ * @param[in] word The word for which you are searching
+ * @param[in] currWordIndex The index to word indicating which letter must
+ *                          be matched by this call
+ */
+bool findWordHelper(const vector<vector<char> >& grid, 
+		 Location currLoc,
+		 Location delta,
+		 string word,
+		 unsigned int currWordIndex)
+{
+  //if letter is not in word, or if it hits the boundary without finishing
+  int row_size = grid.size() ;
+  int col_size = (grid[0]).size() ;
+
+  //Check if bounds:
+  if (currLoc.row < 0 || currLoc.col < 0){
+    return false ;
+  }
+  if (currLoc.row >= row_size || currLoc.col >= col_size){
+    return false ;
+  }
+
+  char currChar = grid[currLoc.row][currLoc.col];
+
+
+  if (word[currWordIndex] == currChar){
+    Location next(currLoc.row + delta.row, currLoc.col + delta.col) ;
+    if (currWordIndex + 1 == word.size()){
+      return true ;
+    }
+    else{
+      return findWordHelper(grid, next, delta, word, currWordIndex + 1) ;
+    }
+  }
+  else{
+    return false ;
+  }
+
+
+
+
+
+
+  
+
+}
+
+int main(int argc, char* argv[])
+{
+  if(argc < 3){
+    cerr << "Please provide a grid filename and targetWord" << endl;
+    return 1;
+  }
+  ifstream ifile(argv[1]);
+  string targetWord(argv[2]);
+  if(ifile.fail()){
+    cerr << "Unable to open input grid file" << endl;
+    return 1;
+  }
+
+  vector<vector<char> > mygrid;
+  if( ! readGrid(ifile, mygrid) ){
+    cerr << "Error reading grid" << endl;
+    ifile.close();
+    return 1;
+  }
+  
+  findWord(mygrid, targetWord);
+  
+  return 0;
+}
